239badf06de8458b202dcf83e16614fdd999817a
Cristiano Souza <ltianosouzal@outlook.com>
2024-01-28 16:54:56 -0300

    fixed: bug encrypt

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,80 +1,85 @@
-const body = document.querySelector('body');
-const imgTexEncry = document.getElementById('img__search');
+function encrypt (text) {
+    return text.replace(/e/g, 'enter')
+               .replace(/i/g, 'imes')
+               .replace(/a/g, 'ai')
+               .replace(/o/g, 'ober')
+               .replace(/u/g, 'ufat')
+}
 
-addEventListener('click', function (event) {
-    if (event.target !== body){
-        decodeText;
-        imgTexEncry.setAttribute('style', 'display: none');
+function decrypt (text) {
+    return text.replace(/enter/g, 'e')
+               .replace(/imes/g, 'i')
+               .replace(/ai/g, 'a')
+               .replace(/ober/g, 'o')
+               .replace(/ufat/g, 'u')
+}
+
+function textUpercase(text) {
+    return /[A-ZÁÉÍÓÚÀÃÕÂÊÎÔÛÄËÏÖÜÇ]/.test(text);
+}
+
+function encryptText () {
+    const textTyped = document.getElementById('decode__text').value;
+    const resEncrypted = document.getElementById('encrypted');
+    const statusDecode = document.getElementById("status");
+    const btnPast = document.getElementById('btn__paste');
+    const alert = document.getElementById('text__alert');
+    const imgSearch = document.getElementById('img__search');
+
+    if ( textUpercase(textTyped)) {
+        alert.setAttribute('style', 'color: red; font-weight: bolder');
+        document.getElementById('decode__text').value = '';
+        return;
+    }       
+   
+    if  (textTyped){
+        let result;
+        result = encrypt(textTyped);
+        resEncrypted.innerText = result;
+        statusDecode.innerHTML = 'Mensagen Criptografada';
+        imgSearch.style.display='none';
+        document.getElementById('decode__text').value = '';
+    } else {
+        statusDecode.setAttribute('style', 'color: red');
+        statusDecode.innerHTML= 'Você precisa digitar alguma palavra';
+        imgSearch.style.display='block';
+        document.getElementById('decode__text').value = '';
     }
-})
 
-function decodeText() {
-    const encodedTextElement = document.getElementById("decode__text");
-    const encodedText = encodedTextElement.value.toLowerCase();
-    const decodedResultElement = document.getElementById("encrypted");
-    const statusDecoder = document.getElementById("status");
-    const imgTexEncry = document.getElementById('img__search');
 
-    const decodedText = decode(encodedText);
+    if (encrypt(textTyped)) {
+        btnPast.setAttribute('style', 'display: block');
+    } else {
+        btnPast.setAttribute('style', 'display: none');
+    }    
+
+}
+
+function decryptText () {
+    const textTyped = document.getElementById('decode__text').value;
+    const resEncrypted = document.getElementById('encrypted');
+    const statusDecode = document.getElementById("status");
+
+    let result;
+
+    if (textTyped) {
+        result = decrypt(textTyped);
+        resEncrypted.innerText = result;
+        statusDecode.innerHTML = "Mensagem Decriptografada!";
+        document.getElementById('decode__text').value = '';
+    }
+
+}
+
+function  pasteText() {
+    const btnPast = document.getElementById('btn__paste');
+    const resEncrypted = document.getElementById('encrypted');
+    const body = document.querySelector('body');
+    const imgSearch = document.getElementById('img__search');
+    const statusDecode = document.getElementById("status");
     
-    decodedResultElement.innerText = decodedText;
-
-    if (decodedText) {
-        imgTexEncry.setAttribute('style', 'display:none');
-        statusDecoder.innerHTML = "Mensagem Criptografada!";
-    }
-
-    if (decodedText === "") {
-        statusDecoder.innerHTML = "Você precisa digitar alguma palavra para criptografar!";
-        statusDecoder.setAttribute('style', 'color: red', 'font-weight: 700')
-    }
+    navigator.clipboard.writeText(resEncrypted.textContent);
+    imgSearch.style.display = 'none';
+    statusDecode.innerHTML = 'Mensagem copiada!';
+    resEncrypted.innerHTML = '';   
 }
-
-function decode(encodedText) {
-    let decodedText = "";
-
-    for (let i = 0; i < encodedText.length; i++) {
-        let currentChar = encodedText[i];
-
-        switch (currentChar) {
-            case 'e':
-                decodedText += "enter";
-                break;
-            case 'i':
-                decodedText += "imes";
-                break;
-            case 'a':
-                decodedText += "ai";
-                break;
-            case 'o':
-                decodedText += "ober";
-                break;
-            case 'u':
-                decodedText += "ufat";
-                break;
-            default:
-                decodedText += currentChar;
-                break;
-        }
-    }
-
-    return decodedText;
-}
-
-
-function copyButton () {
-    const encodedTextElement = document.getElementById("decoder__text");
-    const encodedText = encodedTextElement.value.toLowerCase();
-    return decode(encodedText);
-}
-
-function encodeText () {
-    const encodedTextElement = document.getElementById("decoder__text");
-    const encodedText = encodedTextElement.value.toLowerCase();
-    const decodedResultElement = document.getElementById("encrypted");
-
-    encodedTextElement.value = "";
-    
-    decodedResultElement.innerHTML = encodedText;
-}
-
